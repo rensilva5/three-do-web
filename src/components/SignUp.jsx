4633dcee47414e7bed3a2ba092b5dd3625544826
rensilva5 <renzoasilva@gmail.com>
2022-09-01 20:13:47 -0400

    SignUp component ready to use

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,10 @@
 import { Modal, Form, Input, Button } from "antd";
 
-export default function SignUp ( { setToken }) {
-    const handleSignUp = ( { email, password }) => {
-        // post request to api.user
-        fetch('http:/localhost:5555/users', {
+export default function SignUp({ setToken }) {
+    const handleSignUp = ({ email, password }) => {
+        // post request to api.users
+        fetch('https://three-do-api-bc.web.app/users', {
+        // fetch('http:/localhost:5555/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -20,21 +21,18 @@ export default function SignUp ( { setToken }) {
     }
     return (
         <Modal title='Create Account' visible closable={false} footer={null}>
-            <Form onFinish={handleSignUp} labelCol={ { span:8 }} wrapperCol={{ span:16 }}>
+            <Form onFinish={handleSignUp} labelCol={{ span:8 }} wrapperCol={{ span:16 }}>
                 <Form.Item label='Email' name='email'>
                     <Input />
                 </Form.Item>
                 <Form.Item label='Password' name='password'>
-                    <Input />
+                    <Input.Password />
                 </Form.Item>
-                <Form.Iteam wrappedCol={{ offset:8, SPAN: 16 }}>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                     <Button type="primary" htmlType="submit" >Sign up</Button>
-                    <Input />
-                </Form.Iteam>
-                <p>Already a  user? <Button onClicktype={'link'}>Login</Button></p>
-
-
-
+                    {/* <Input /> */}
+                </Form.Item>
+                <p>Already a  user? <Button onClick={() => setIsUser(true)} type={'link'}>Login</Button></p>
             </Form>
         </Modal>
     )
