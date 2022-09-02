70ed464e93b8ad119ca3e17d3f9b9952c38092b8
rensilva5 <renzoasilva@gmail.com>
2022-09-01 20:20:46 -0400

    TodoList is ready to be used

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,39 +1,50 @@
 import { Modal, Form, Input, Button } from "antd";
 
 export default function SignUp({ setToken }) {
-    const handleSignUp = ({ email, password }) => {
-        // post request to api.users
-        fetch('https://three-do-api-bc.web.app/users', {
-        // fetch('http:/localhost:5555/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password })
-        })
-        .then(response => response.json())
-        .then(data => {
-            setToken(data.token)
-            localStorage.setItem('token', data.token)
-        })
-        .catch(err => alert(err.message))
-        //set token
-    }
-    return (
-        <Modal title='Create Account' visible closable={false} footer={null}>
-            <Form onFinish={handleSignUp} labelCol={{ span:8 }} wrapperCol={{ span:16 }}>
-                <Form.Item label='Email' name='email'>
-                    <Input />
-                </Form.Item>
-                <Form.Item label='Password' name='password'>
-                    <Input.Password />
-                </Form.Item>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" htmlType="submit" >Sign up</Button>
-                    {/* <Input /> */}
-                </Form.Item>
-                <p>Already a  user? <Button onClick={() => setIsUser(true)} type={'link'}>Login</Button></p>
-            </Form>
-        </Modal>
-    )
+  const handleSignUp = ({ email, password }) => {
+    // post request to api.users
+    fetch("https://three-do-api-bc.web.app/users", {
+      // fetch('http:/localhost:5555/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+      })
+      .catch((err) => alert(err.message));
+    //set token
+  };
+  return (
+    <Modal title="Create Account" visible closable={false} footer={null}>
+      <Form
+        onFinish={handleSignUp}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+      >
+        <Form.Item label="Email" name="email">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Sign up
+          </Button>
+          {/* <Input /> */}
+        </Form.Item>
+        <p>
+          Already a user?{" "}
+          <Button onClick={() => setIsUser(true)} type={"link"}>
+            Login
+          </Button>
+        </p>
+      </Form>
+    </Modal>
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,47 +3,44 @@ import { List, Alert } from "antd";
 import TodoListCard from "./TodoListCard";
 
 export default function TodoList({ tasklist, setTasklist, token }) {
-  const [loading, setLoading ] = useState(true)
-  const [error, setError] = useState()
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
   // call the api and use setTaskLits to fill in state
   useEffect(() => {
-    // fetch('https://three-do-api-rs.web.app/tasks', {
-    fetch('http:localhost:/tasks', {
+    fetch("https://three-do-api-rs.web.app/tasks", {
+      // fetch('http:localhost:/tasks', {
       heasders: {
-        'Authorization': token,
-      }
+        Authorization: token,
+      },
     })
-      .then(results => results.json())
-      .then(tasks => {
-        setTasklist(tasks)
-        setLoading(false)
-        setError('')
+      .then((results) => results.json())
+      .then((tasks) => {
+        setTasklist(tasks);
+        setLoading(false);
+        setError("");
       })
-      .catch(err => {
-        setError(err.mesage)
-        setLoading(false)
+      .catch((err) => {
+        setError(err.mesage);
+        setLoading(false);
       });
   }, [token, setTasklist, setLoading, setError]);
   return (
     <>
-    {error && token} <Alert
-        message="Error"
-        description={error}
-        type="error"
-        showIcon
-        />
-    <div className='task-list'>
+      {error && token && (
+        <Alert message="Error" description={error} type="error" showIcon />
+      )}
+      <div className="task-list">
         <List
           dataSource={tasklist}
           loading={loading}
           renderItem={(item) => (
-            <TodoListCard 
-            key={item.id}
-            item={item}
-            token={token}
-            setLoading={setLoading}
-            setTasklist={setTasklist}
-            setError={setError}
+            <TodoListCard
+              key={item.id}
+              item={item}
+              token={token}
+              setLoading={setLoading}
+              setTasklist={setTasklist}
+              setError={setError}
             />
           )}
         />
